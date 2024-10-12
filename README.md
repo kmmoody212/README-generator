541abff4eecb105f17b624220b79de8603a619c3
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-10-11 21:39:38 -0400

    Created questions for README generation, wrote functions to write the code to a README file.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# README-generator
+# README Generator
+
+  ## Description
+  An app used to generate a simple README template.
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+  Install npm with "npm install" or "npm i"
+
+  ## Usage
+  You will need to use Node.js
+
+  ## Credits
+  undefined
+
+  ## License
+    This project is licensed under the MIT license.
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  [MIT License](https://opensource.org/licenses/MIT)
--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+import fs from "fs";
+import inquirer from "inquirer";
+import generateMarkdown from "./generateMarkdown.js";
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// An array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is your project's title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please write a description of your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "How do you use this project?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What are the steps to installing your project?",
+    name: "installation",
+  },
+  {
+    type: "list",
+    message: "Please select a license.",
+    name: "license",
+    choices: ["MIT", "Apache 2.0", "BSD 3-Clause"],
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error("Error writing file", err);
+    } else {
+      console.log("README file created successfully!");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    const markdownContent = generateMarkdown(answers);
+    writeToFile("README.md", markdownContent);
+  } catch (error) {
+    console.error("Error initializing the app", error);
+  }
+}
 
 // Function call to initialize app
 init();
